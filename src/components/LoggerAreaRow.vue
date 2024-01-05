673424ebf72b568caa4bfd19be4ec1d6ab94e2d2
Stephan Ploehn <stephan@foerdeliebe.com>
2024-01-05 16:36:52 +0100

    feat: visual update for kirby 4 and make use of v-code-diff

---
+++ src/components/LoggerAreaRow.vue
@@ -0,0 +1,58 @@
+<template>
+    <tbody>
+        <tr>
+            <td>{{ log.timestamp }}</td>
+            <td>{{ log.user }}</td>
+            <td>{{ log.type }}</td>
+            <td>{{ log.action }}</td>
+            <td>{{ log.slug }}</td>
+            <td>{{ log.language }}</td>
+            <td>
+                <k-button-group layout="collapsed">
+                    <k-button variant="filled" :icon="toggleIcon" v-on:click="toggleActive()">
+                        {{ toggleLabel }}
+                    </k-button>
+                </k-button-group>
+            </td>
+        </tr>
+        <tr v-if="isActive">
+            <td colspan="7">
+            <code-diff
+                :old-string="log.oldData"
+                :new-string="log.newData"
+                output-format="side-by-side"
+                language="json"
+                context="3"
+                trim="true"
+            />
+            </td>
+        </tr>
+    </tbody>
+</template>
+
+<script>
+export default {
+    name: 'LoggerAreaRow',
+    props: ['log'],
+    data() {
+        return {
+            isActive: false,
+            toggleLabel: 'Show diff',
+            toggleIcon: 'add'
+        }
+    },
+    methods: {
+        toggleActive() {
+            this.isActive = !this.isActive;
+
+            if (this.isActive) {
+                this.toggleLabel = 'Hide diff';
+                this.toggleIcon = 'remove';
+            } else {
+                this.toggleLabel = 'Show diff';
+                this.toggleIcon = 'add';
+            }
+        }
+    }
+}
+</script>
